b3a1c7fc14a171e25df143b81d1f6283c3a7ab3b
Abhinav kumar <42245122+singhdotabhinav@users.noreply.github.com>
2020-08-25 17:42:28 +0530

    Create Iterative_Soln.cpp

---
+++ Longest_Common_Subsequence/Iterative_Soln.cpp
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+#include<iostream>
+#include<math.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+int LCS( string X, string Y, int m, int n ){
+
+    int t[m+1][n+1];
+    for(int i=0 ; i<=m ; i++ ){
+        for(int j=0 ; j<=n ; j++){
+            if(i==0 || j==0)
+                t[i][j]=0;
+            else if (X[i - 1] == Y[j - 1])
+                t[i][j] = t[i - 1][j - 1] + 1;
+
+            else
+                t[i][j] = max(t[i - 1][j], t[i][j - 1]);
+        }
+    }
+    return t[m][n];
+}
+
+
+int main()
+{
+    string X = "AGGTAB";
+    string Y = "GXTXAYB";
+
+
+    int M = X.size();
+    int N = Y.size();
+
+    int t[M+1][N+1];
+    memset(t, -1, sizeof(t));
+
+
+    cout<<"Length of LCS is "<< LCS( X, Y, M, N ) ;
+
+    return 0;
+}
