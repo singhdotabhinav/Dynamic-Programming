44dd3cd8fec2fd29c58276a517c9038a35cd498f
Abhinav kumar <42245122+singhdotabhinav@users.noreply.github.com>
2020-08-25 11:55:24 +0530

    Update Target_sum.cpp

--- Target_sum.cpp
+++ Target_sum.cpp
@@ -64,3 +64,49 @@ int main()
 
     return 0;
 }
+
+
+
+
+//Leet Code Solution && Covering some basic edge cases!!
+//Cnt is for '0's in the array. If we have n number of zeroes then we need to multiply out result of count of subsets with a sum to 2^n.  And we need to not consider this while calculating in bottom-up manner, hence the case " if(nums[i-1]==0) { dp[i][j] = dp[i-1][j] } ".
+//For example: If we have 2(cnt=2) zeroes in the example array for which we calculate count of subset with a sum then there are 4(2^cnt) ways that is either selecting first zero, second selecting second 0 present in array, third not selecting both, fourth selecting both.
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int S)
+    {
+        int cnt = 0,sum = 0;
+        int n = nums.size();
+        for(int i=0;i<nums.size();i++)
+        {
+            sum = sum+nums[i];
+            if(nums[i]==0)
+                cnt = cnt+1;
+        }
+        if(S > sum)
+            return 0;
+        if((S+sum)%2!=0)
+            return 0;
+        int s = (S+sum)/2;
+        int dp[n+1][s+1];
+        for(int i=0;i<n+1;i++)
+            dp[i][0] = 1;
+        for(int j=1;j<s+1;j++)
+            dp[0][j] = 0;
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<s+1;j++)
+            {
+                if(nums[i-1]==0)
+                    dp[i][j] = dp[i-1][j];
+                else if(j < nums[i-1])
+                    dp[i][j] = dp[i-1][j];
+                else
+                    dp[i][j] = dp[i-1][j] + dp[i-1][j-nums[i-1]];
+            }
+        }
+        
+        return pow(2,cnt)*dp[n][s];
+    }
+};
+
