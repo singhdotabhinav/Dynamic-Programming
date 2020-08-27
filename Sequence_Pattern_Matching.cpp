ea3da0dfe93a0345ac1dea874c99dba21a37c4b0
Abhinav kumar <42245122+singhdotabhinav@users.noreply.github.com>
2020-08-27 10:10:08 +0530

    Create Sequence_Pattern_Matching.cpp

---
+++ Sequence_Pattern_Matching.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include<iostream>
+#include<math.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int LCS( string X, string Y, int m, int n ){
+
+    int t[m+1][n+1];
+    for(int i=0 ; i<=m ; i++ ){
+        for(int j=0 ; j<=n ; j++){
+            if(i==0 || j==0)
+                t[i][j]=0;
+            else if (X[i - 1] == Y[j - 1])
+                t[i][j] = t[i - 1][j - 1] + 1;
+
+            else
+                t[i][j] = max(t[i - 1][j], t[i][j - 1]);
+        }
+    }
+    return t[m][n];
+}
+
+int main()
+{
+    string X = "AXY";
+    string Y = "ADXCY";
+
+
+    int M = X.size();
+    int N = Y.size();
+
+
+    bool a;
+    int b=LCS( X, Y, M, N ) ;
+
+    if(b==M)
+        a=true;
+    else
+        a=false;
+
+    cout<<"Length of Sequence pattern matching is "<< a ;
+
+    
+
+
+
+    return 0;
+}
