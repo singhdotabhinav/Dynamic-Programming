c8fc614d6670e6bea1cbc3a2bb41dee010247475
Abhinav kumar <42245122+singhdotabhinav@users.noreply.github.com>
2020-08-30 14:58:40 +0530

    Update Egg_Dropping_Problem.cpp

--- Egg_Dropping_Problem.cpp
+++ Egg_Dropping_Problem.cpp
@@ -19,4 +19,41 @@ int main(){
 
     cout<<solve(2,6);
 }
+//Recursive + memoized
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int t[100][10000];
+int solve(int e, int f){
+    if(e==1)
+        return f;
+    if(f==0||f==1)
+        return f;
+    if(t[e][f]!=-1)
+        return t[e][f];
+
+    int mn=INT_MAX;
+
+    for(int k=1;k<f;k++){
+
+
+        int temp= 1 + max(solve(e-1,k-1),solve(e,f-k));
+        mn=min(temp,mn);
+    }
+
+    return t[e][f]=mn;
+
+}
+
+int main(){
+
+    //int e=4;
+    //int f=3;
+    //int t[e+1][f+1];
+    memset(t,-1,sizeof(t));
+
+    cout<<solve(3,14);
+}
+
 
