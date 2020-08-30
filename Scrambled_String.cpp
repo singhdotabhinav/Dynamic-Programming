c1b7662716cc05a222134c2f98affa630d589f61
Abhinav kumar <42245122+singhdotabhinav@users.noreply.github.com>
2020-08-30 12:24:14 +0530

    Create Scrambled_String.cpp

---
+++ Scrambled_String.cpp
@@ -0,0 +1,42 @@
+
+unordered_map<string, bool> mp;
+class Solution {
+public:
+    bool isScramble(string s1, string s2){
+        
+        if(s1.length()!= s2.length())
+            return false;
+        
+        if(s1.length() && s2.length() == 0)
+            return true;
+    
+        if(s1.compare(s2) == 0)
+            return true;
+        
+        if(s1.length() <= 1 || s2.length()<=1)
+            return false;
+        
+        string key = s1;     
+        key.push_back('$');
+        key.append(s2);
+        
+        if(mp.find(key) != mp.end())
+            return mp[key];
+        
+        int n = s1.length();
+        
+        bool flag = false;
+    
+        for(int i = 1; i < n; i++){
+        
+            bool c1 = (isScramble(s1.substr(0, i),s2.substr(n - i, i)) && isScramble(s1.substr(i, n-i), s2.substr(0, n - i)));
+            bool c2 = (isScramble(s1.substr(0, i),s2.substr(0, i)) && isScramble(s1.substr(i, n-i), s2.substr(i,n - i)));                                                     
+        
+              if(c1 || c2){
+                 flag = true;
+                 break;
+              }
+        }
+         return mp[key] = flag;
+    }
+};
